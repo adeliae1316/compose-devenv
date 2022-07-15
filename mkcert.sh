6f93cc3c7ed46865d981b9bf0c71a8829a01565b
adeliae1316 <knsak.ug487@gmail.com>
2022-07-16 08:18:10 +0900

    organize shell processing

--- mkcert.sh
+++ mkcert.sh
@@ -6,41 +6,23 @@ set -o allexport
 source "${CURRENT_DIR}"/.env
 set +o allexport
 
-
-if [ -n "$(docker container ls -a -q -f name=mkcert)" ]; then
-    docker container rm -f mkcert &> /dev/null
-fi
-
+# reduction of build context submission
 cat "${CURRENT_DIR}/mkcert/Dockerfile" | docker build -t mkcert -
 
-docker run \
-    --env _UID=${UID} \
-    --env _USER=${USER} \
+docker run --rm \
+    --env _UID=${UID} --env _USER=${USER} \
+    --env CA_NAME="${_LDAP_ORGANISATION} self-signed CA" \
+    --env CERT_NAME="${_HOSTNAME}" \
     -v "${CURRENT_DIR}/mkcert/rootcerts:/home/${USER}/.local/share/mkcert" \
     -v "${CURRENT_DIR}/mkcert/usercerts:/home/${USER}/certificates" \
     -v "${CURRENT_DIR}/mkcert/useradd.sh:/tmp/entrypoint.sh:ro" \
-    --init --interactive --tty --detach \
-    --name mkcert \
+    --init --interactive --tty \
     --entrypoint /tmp/entrypoint.sh \
+    --workdir /home/${USER}/certificates \
     mkcert \
-    bash
-
-docker exec \
-    --interactive --tty --user ${UID} \
-    mkcert \
-    sudo chown -R "${USER}:${USER}" ${HOME}
-
-docker exec \
-    --env _MKCERT_CA_OU='Workspace self-signed CA' \
-    --interactive --tty --user ${UID} --workdir /home/${USER}/certificates \
-    mkcert \
-    mkcert -install
-
-docker exec \
-    --env _MKCERT_CERT_OU="${_HOSTNAME}" \
-    --interactive --tty --user ${UID} --workdir /home/${USER}/certificates \
-    mkcert \
-    mkcert localhost "${_HOSTNAME}" 127.0.0.1
+    bash -c 'sudo chown -R "${_UID}:${_UID}" /home/${_USER} \
+            && _MKCERT_CA_OU=${CA_NAME} mkcert -install \
+            && _MKCERT_CERT_OU=${CERT_NAME} mkcert localhost "${CERT_NAME}" 127.0.0.1'
 
 (
     DST_DIR="${CURRENT_DIR}/mkcert/certs"
@@ -51,7 +33,7 @@ docker exec \
 
     chmod 600 "${CURRENT_DIR}/mkcert/rootcerts/rootCA-key.pem"
     cp "${CURRENT_DIR}/mkcert/rootcerts/rootCA-key.pem" "${DST_DIR}/readonly-root.key"
-    chmod 400 "${CURRENT_DIR}/mkcert/rootcerts/rootCA-key.pem" "${DST_DIR}/readonly-root.key"
+    # chmod 400 "${CURRENT_DIR}/mkcert/rootcerts/rootCA-key.pem" "${DST_DIR}/readonly-root.key"
 
     SRC_CRT="${CURRENT_DIR}/mkcert/usercerts/localhost+2.pem"
     SRC_KEY="${CURRENT_DIR}/mkcert/usercerts/localhost+2-key.pem"
