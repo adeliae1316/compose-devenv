cf374d558bcdbe482b98bec4a096158b71c93d5c
adeliae1316 <knsak.ug487@gmail.com>
2022-07-16 08:16:58 +0900

    organize installation modules

--- mkcert/useradd.sh
+++ mkcert/useradd.sh
@@ -2,6 +2,11 @@
 
 # need to pass following variables: _UID as user id, _USER as username.
 
+apt update \
+    && apt install -y sudo gosu \
+    && apt clean \
+    && rm -rf /var/lib/apt/lists/*
+
 groupadd --gid ${_UID} ${_USER} \
     && useradd --uid ${_UID} --gid ${_UID} --create-home ${_USER} &> /dev/null \
     && echo "${_USER}:${_USER}" | chpasswd \
